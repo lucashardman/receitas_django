edf023d0884bf6549795b964a289776863c7de27
Lucas Hardman <lucas.hardman92@gmail.com>
2021-04-30 21:55:40 -0300

    init mysql

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa',
+        3:'Sorvete ',
+        4:'Bolo'
+    }
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request, 'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
